fe8552bfcb075a6d5c964be47c49bba0753ed321
EC2 Default User <ec2-user@ip-172-31-12-163.ap-northeast-1.compute.internal>
2023-03-26 10:29:57 +0000

    Task5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:top]
+  before_action :authenticate_admin_and_user!, except: [:top]
+  #before_action :authenticate_user!, except: [:top]
+  private
+  def authenticate_admin_and_user!
+    if admin_signed_in?
+      true
+    else
+      authenticate_user!
+    end
+  end
 end
--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,15 @@
 class Public::HomesController < ApplicationController
   def top
-    @game_informations = GameInformation.all
+    game_information_all = GameInformation.all
+    @game_informations = {}
+    # [Sun, 05 Mar 2023, Sun, 26 Mar 2023]
+    GameInformation.pluck(:game_day).uniq.each do |day|
+      game_information_all.each do |game_info|
+        if game_info.game_day == day
+          @game_informations[day] = [] if @game_informations[day].nil?
+          @game_informations[day] << game_info
+        end
+      end
+    end
   end
 end
