9f7690da0d2f0888e9235f0eefb7e5adf64ed179
EC2 Default User <ec2-user@ip-172-31-12-163.ap-northeast-1.compute.internal>
2023-03-27 13:56:20 +0000

    Task5

--- app/controllers/admin/game_information_controller.rb
+++ app/controllers/admin/game_information_controller.rb
@@ -14,10 +14,17 @@ class Admin::GameInformationController < ApplicationController
   end
 
   def index
-    @game_informations = GameInformation.all
-    #byebug
-    #@first_team = @game_informations.first_team
-    #@second_team = @game_informations.second_team
+    game_information_all = GameInformation.all
+    @game_informations = {}
+    # [Sun, 05 Mar 2023, Sun, 26 Mar 2023]
+    GameInformation.pluck(:game_day).uniq.each do |day|
+      game_information_all.each do |game_info|
+        if game_info.game_day == day
+          @game_informations[day] = [] if @game_informations[day].nil?
+          @game_informations[day] << game_info
+        end
+      end
+    end
   end
 
   def edit
